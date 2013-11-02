5b28ee10d8159505031d8e7a2ce9d76ad9ac3055
zava <zava.kid@gmail.com>
2013-11-02 15:18:12 +0800

    first init

---
+++ build.sbt
@@ -0,0 +1,19 @@
+name := "mushroom2"
+
+version := "0.1"
+
+organization := "com.zavakid.mushroom2"
+
+scalaVersion := "2.10.2"
+
+resolvers ++= Seq(
+    "snapshots" at "http://oss.sonatype.org/content/repositories/snapshots",
+    "releases"  at "http://oss.sonatype.org/content/repositories/releases"
+    )
+
+libraryDependencies ++= Seq(
+  "org.scalatest" %% "scalatest" % "1.9.2" % "test",
+  "org.specs2" %% "specs2" % "2.3.1" % "test"
+  )
+
+EclipseKeys.withSource := true
---
+++ src/main/scala/com/zavakid/mushroom/scala/models.scala
@@ -0,0 +1,39 @@
+package com.zavakid.mushroom.scala
+
+/**
+ * @author zavakid 2013年11月2日 下午3:27:16
+ */
+sealed trait Metric[T] {
+  def name: String
+  def desc: String = "<<no description>>"
+  def value: T
+}
+
+/** 表示计数的 metric */
+case class MetricCounter[T](val name: String, override val desc: String, val value: T) extends Metric[T]
+/** 表示差值的 metric */
+case class MetricDeltal[T](val name: String, override val desc: String, val value: T) extends Metric[T]
+
+trait MetricProvider {
+  def getMetricsRecord(all: Boolean): MetricsRecord
+}
+
+/** metric 记录，每个 provider 每次提供一个 */
+case class MetricsRecord(val name:String, val desc: String, val metrics: Seq[Metric[_]])
+
+/** metric 消费者 */
+trait MetricsSink {
+  def putMetrics(record: MetricsRecord)
+  def flush
+}
+
+trait MetricsSystem {
+  /** 注册一个组件。比如 MetricProvider 或者 MetricsSink */
+  def register[T](name: String, desc: String, component: T)
+
+  /** 注册一个不存在的组件。比如 MetricProvider 或者 MetricsSink，如果存在的话，会返回原来老的组件 */
+  def registerIfAbsent[T](name: String, desc: String, component: T): T
+
+  def start
+  def shutdown
+}
